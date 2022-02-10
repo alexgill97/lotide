9a303e2c30dc1f798ab510bc5d89b393ed339303
Alex <your@email.com>
2022-02-10 23:08:45 +0000

    adding findKey.js file

---
+++ takeUntil.js
@@ -0,0 +1,33 @@
+let assertArraysEqual = function(actual, expected) {
+  const result = eqArrays(actual, expected);
+  if (result === true) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let output = true;
+  for (let i = 0; i < 0; i++) {
+    if (arr1[i] !== arr2[i]) {
+      output = false;
+      break;
+    }
+  }
+  return output;
+};
+
+const takeUntil = function(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) return array.splice(0, i);
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
